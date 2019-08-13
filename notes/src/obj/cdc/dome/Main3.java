24d982f1dcbfb29939121351047102b3dbbabb04
wjf_default <2399012862@qq.com>
2019-08-13 15:30:21 +0800

    java笔记

--- notes/src/obj/cdc/dome/Main3.java
+++ notes/src/obj/cdc/dome/Main3.java
@@ -2,9 +2,39 @@ package obj.cdc.dome;
 
 /**
  * @program: notes
- * @description: 构造方法的使用
+ * @description: 构造方法的使用, 调用其他构造方法，代码复用
  * @author: wujf
  * @create: 2019-08-13 14:27
  **/
 public class Main3 {
+  public static void main(String[] args) {
+    Dog dog = new Dog();
+    System.out.println(dog.getName()+dog.getAge());
+  }
+}
+
+class Dog {
+  private String name;
+  private int age;
+
+  public Dog(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Dog(String name) {
+    this(name, 18); //调用另一个构造方法Dog(String, int)
+  }
+
+  public Dog() {
+    this("秋田犬"); // 调用另一个构造方法Dog(String)
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
 }
--- notes/src/obj/cdc/dome/Main5.java
+++ notes/src/obj/cdc/dome/Main5.java
@@ -2,9 +2,52 @@ package obj.cdc.dome;
 
 /**
  * @program: notes
- * @description: 继承
+ * @description: 类的继承
  * @author: wujf
  * @create: 2019-08-13 15:08
  **/
 public class Main5 {
+  public static void main(String[] args) {
+
+  }
+}
+// 仔细观察，发现Student类包含了Person类已有的字段和方法，只是多出了一个score字段和相应的getScore()、setScore()方法。能不能在Student中不要写重复的代码？
+class Person1 {
+  private String name;
+  private int age;
+  public String getName() {
+    return this.name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return this.age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
+class Student {
+  private String name;
+  private int age;
+  private double score;
+    public String getName() {
+    return this.name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return this.age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public double getScore() {
+      return this.score;
+  }
+  public void setScore(double score) {
+      this.score = score;
+  }
 }
