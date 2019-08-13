9a49d9419074466c341b219018e4bfe7929aa819
wjf_default <2399012862@qq.com>
2019-08-13 19:04:56 +0800

    java笔记

--- notes/src/obj/cdc/dome/Main7.java
+++ notes/src/obj/cdc/dome/Main7.java
@@ -8,6 +8,65 @@ package obj.cdc.dome;
  **/
 public class Main7 {
   public static void main(String[] args) {
-
+    Animate p = new Pig();
+    p.run(); // pig run
+    p.run(); // pig run
+    Income[] incomes = new Income[] {
+        new Income(3000),
+        new SalaryIncome(7000),
+        new StateCouncilSpecialAllowance(15000)
+    };
+    System.out.println(totalIncome(incomes));
+  }
+  public static double totalIncome(Income... incomes) {
+    double total = 0;
+    for (Income income: incomes) {
+      total += income.getIncome();
+    }
+    return total;
+  };
+}
+class Animate {
+  public void run() {
+    System.out.println("animate run");
+  }
+}
+class Pig extends Animate {
+  @Override
+  public void run() {
+    System.out.println("pig run");
+  }
+}
+// Income 税收
+class Income {
+  protected double income;
+  public double getIncome() {
+    return income * 0.1;
+  }
+  public Income(double income) {
+    this.income = income;
+  }
+}
+// 税收基数
+class SalaryIncome extends Income{
+  public SalaryIncome(double income) {
+    super(income);
+  }
+  @Override
+  public double getIncome() {
+    if(income < 5000) {
+      return 0;
+    }
+    return (income - 5000) * 0.2;
+  }
+}
+//StateCouncilSpecialAllowance 免税
+class StateCouncilSpecialAllowance extends Income {
+  public StateCouncilSpecialAllowance(double income) {
+    super(income);
+  }
+  @Override
+  public double getIncome() {
+    return 0;
   }
 }
